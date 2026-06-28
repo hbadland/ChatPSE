ca7534180853f3081bbdc034ee2bdce95ce7adfe
hbadland <harrybadland@outlook.com>
2026-06-28 20:24:25 +0100

    Sync script: rsync whole tree, exclude results/deps/sif/tar.gz, no --delete

--- sync_to_hpc.sh
+++ sync_to_hpc.sh
@@ -3,16 +3,20 @@ set -euo pipefail
 HPC="hgb25@login.cx3.hpc.imperial.ac.uk"
 DEST="/rds/general/user/hgb25/home/multiAgentFlowsheet"
 
-rsync -avz --delete \
+rsync -avz \
     --exclude 'results/' \
     --exclude 'deps/' \
     --exclude '__pycache__/' \
     --exclude '*.pyc' \
     --exclude '*.sif' \
     --exclude '*.dwxmz' \
+    --exclude '*.tar.gz' \
+    --exclude '*.tar' \
+    --exclude '*.zip' \
     --exclude '.git/' \
     --exclude '.ollama/' \
     --exclude '*.log' \
+    --exclude '.DS_Store' \
     ./ "$HPC:$DEST/"
 
 echo "Synced $(git branch --show-current) to HPC."
